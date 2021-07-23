bb233e8acbbc5f4ae5f1c0242c6069b41d0f268c
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2021-07-23 10:44:11 -0500

    navbar components and styled components

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,19 @@
+ import React from 'react'
+ import { Nav, NavbarContainer, NavLogo } from './NavbarElements'
+ 
+ const Navbar = () => {
+    return (
+    <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>Dolla </NavLogo>
+            </NavbarContainer>
+        </Nav>
+    </>
+    )
+ }
+ 
+ export default Navbar;
+ 
+
+
