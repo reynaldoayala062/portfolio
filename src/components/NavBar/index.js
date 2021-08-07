7c59efbb56e2e6d688df3dd506247f54ec1a1080
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2021-08-07 12:57:24 -0500

    soft scroll finished

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,6 @@
  import React, { useState, useEffect } from 'react'
  import {FaBars} from 'react-icons/fa'
+ import {animateScroll as scroll} from 'react-scroll'
  import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements'
 
  
@@ -18,27 +19,31 @@
         window.addEventListener('scroll', changeNav)
     }, [])
 
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    }
+
 
     return (
     <>
         <Nav scrollNav={scrollNav}>
             <NavbarContainer>
-                <NavLogo to='/'>Reynaldo Ayala</NavLogo>
+                <NavLogo to='/' onClick={toggleHome}>Reynaldo Ayala</NavLogo>
                 <MobileIcon onClick={toggle}>
                     <FaBars />
                 </MobileIcon>
                 <NavMenu>
                     <NavItem>
-                        <NavLinks to="about"> About </NavLinks>
+                        <NavLinks to='about' smooth={true} duration={500} spy={true} exact='true' offset={-80}> About </NavLinks>
                     </NavItem>
                     <NavItem>
-                        <NavLinks to="projects"> Projects </NavLinks>
+                        <NavLinks to="projects" smooth={true} duration={500} spy={true} exact='true' offset={-80}> Projects </NavLinks>
                     </NavItem>
                     <NavItem>
-                        <NavLinks to="skills"> Skills </NavLinks>
+                        <NavLinks to="services" smooth={true} duration={500} spy={true} exact='true' offset={-80}> Service </NavLinks>
                     </NavItem>
                     <NavItem>
-                        <NavLinks to="signup"> Sign Up </NavLinks>
+                        <NavLinks to="skills" smooth={true} duration={500} spy={true} exact='true' offset={-80}> Skills </NavLinks>
                     </NavItem>
                 </NavMenu>
                 <NavBtn>
