17ace479fbc57efa1a9037af0efa804f5a933b09
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2021-08-24 21:21:25 -0500

    edit mobile service section

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -13,11 +13,11 @@ export const ServicesContainer = styled.div`
     }
 
     @media screen and (max-width: 786px) {
-        height: 1300px;
+        height: 2400px;
     }
 
     @media screen and (max-width: 480px) {
-        height: 2300px;
+        height: 2400px;
     }
 `;
 
